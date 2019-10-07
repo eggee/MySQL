225b1260711018e8290236dbadb4e14c2e051730
Eggy <timothy.eggenberger@adtran.com>
2019-10-07 16:48:18 -0500

    Tidy-up script added comments.

--- TestbedDB/CreateTestbedEntry.py
+++ TestbedDB/CreateTestbedEntry.py
@@ -1,37 +1,37 @@
+"""
+This script adds entries to the photon_testbeds table in the 'Inventory' database.
+database on phpMyAdmin at 10.21.1.181 at work.
+https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
+"""
+
 from __future__ import print_function
-from datetime import date, datetime, timedelta
 import mysql.connector
 
+#connect to the database and store connecting into 'cnx'
 cnx = mysql.connector.connect(user='bbdlc', password='bbdlc',
                               host='10.21.1.181',
                               database='Inventory')
+
+#create a new cursor, by default a MySQLCursor object, using the connection's cursor() method
 cursor = cnx.cursor()
 
-tomorrow = datetime.now().date() + timedelta(days=1)
-
+# The information of the new employee is stored in the tuple data_employee.
 add_tb_entry = ("INSERT INTO photon_testbeds "
                "(tb_name, device, part_no, serial_no, ip_addr) "
                "VALUES (%s, %s, %s, %s, %s)")
-# add_salary = ("INSERT INTO salaries "
-#               "(emp_no, salary, from_date, to_date) "
-#               "VALUES (%(emp_no)s, %(salary)s, %(from_date)s, %(to_date)s)")
 
+#dummy info to test insterting testbed information
 data_tb_entry = ('MCI-1', 'OLT-1', '1187xxxx', 'ADTN12345678', '10.13.245.321')
 
-# Insert new employee
+# Insert new testbed entry
+#The query to insert the new employee is executed and we
 cursor.execute(add_tb_entry, data_tb_entry)
+
+#retrieve the newly inserted value for the indx_no column
+# (an AUTO_INCREMENT column) using the lastrowid property of the cursor object.
 indx_no = cursor.lastrowid
 
-# Insert salary information
-# data_salary = {
-#   'emp_no': emp_no,
-#   'salary': 50000,
-#   'from_date': tomorrow,
-#   'to_date': date(9999, 1, 1),
-# }
-# cursor.execute(add_salary, data_salary)
-
-# Make sure data is committed to the database
+#it is necessary to commit your changes using the connection's commit() method.
 cnx.commit()
 
 cursor.close()
